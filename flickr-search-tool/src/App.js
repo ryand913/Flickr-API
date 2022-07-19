2c771dafc78d74ef8ef1b655cdd34cccbe33c794
ryand913 <98771607+ryand913@users.noreply.github.com>
2022-07-18 21:11:10 -0400

    Project set up

---
+++ flickr-search-tool/src/App.js
@@ -0,0 +1,10 @@
+
+import './index.css';
+import apiKey from './config'
+function App() {
+  return (
+    console.log('test')
+  );
+}
+
+export default App;
---
+++ flickr-search-tool/src/config.js
@@ -0,0 +1,2 @@
+const apiKey = '7dda89cc339e4dc5c0f2fa83714bafbd';
+export default apiKey;
